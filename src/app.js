278b0c87f1d9ed2c7f626a082f364f4bf12baa77
s4nt14go <santiagopergamo@gmail.com>
2022-07-10 11:36:47 -0300

    feat: add prettier and husky

--- src/app.js
+++ src/app.js
@@ -1,21 +1,26 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const {sequelize} = require('./model')
-const {getProfile} = require('./middleware/getProfile')
+const { sequelize } = require('./model');
+const { getProfile } = require('./middleware/getProfile');
+const {Op} = require("sequelize");
 const app = express();
 app.use(bodyParser.json());
-app.set('sequelize', sequelize)
-app.set('models', sequelize.models)
+app.set('sequelize', sequelize);
+app.set('models', sequelize.models);
 
 /**
  * @returns contract by id
  */
-app.get('/contracts/:id',getProfile ,async (req, res) =>{
-    const {Contract} = req.app.get('models')
-    const {id} = req.params
-    const contract = await Contract.findOne({where: {id}})
-    console.log('contract found:', contract.get())
-    if(!contract) return res.status(404).end()
-    res.json(contract)
-})
+app.get('/contracts/:id', getProfile, async (req, res) => {
+  const { Contract } = req.app.get('models');
+  const { id } = req.params;
+  const contract = await Contract.findOne({
+    where: {
+      id,
+      [Op.or]: [{ ContractorId: req.profile }, { ClientId: req.profile }],
+    },
+  });
+  if (!contract) return res.status(404).end();
+  res.json(contract);
+});
 module.exports = app;
