f6319a8dfc7df9e6fadf4cad6821f283d21ba862
s4nt14go <santiagopergamo@gmail.com>
2022-07-11 01:16:25 -0300

    feat: get the best profession between time range

--- src/app.js
+++ src/app.js
@@ -215,4 +215,51 @@ app.post('/balances/deposit/:userId', async (req, res) => {
   res.json();
 });
 
+/**
+ * Returns the profession that earned the most money (sum of jobs paid) for any contactor that worked in the query time range
+ * @returns profession
+ */
+app.get('/admin/best-profession', async (req, res) => {
+  const { start, end } = req.query;
+
+  const { Profile, Contract, Job } = req.app.get('models');
+
+  const jobs = await Job.findAll({
+    attributes: [[sequelize.fn('sum', sequelize.col('price')), 'totalPaid']],
+    order: [[sequelize.fn('sum', sequelize.col('price')), 'DESC']],
+    group: ['Contract.Contractor.profession'],
+    limit: 1,
+    where: {
+      paid: true,
+      paymentDate: {
+        [Op.between]: [start, end],
+      },
+    },
+    include: [
+      {
+        model: Contract,
+        include: [
+          {
+            model: Profile,
+            as: 'Contractor',
+            where: { type: 'contractor' },
+            attributes: ['profession'],
+          },
+        ],
+      },
+    ],
+  });
+
+  if (!jobs.length) return res
+      .status(404)
+      .send({
+        message: `No paid jobs found between ${start} and ${end}`,
+      })
+      .end();
+
+  res.json({
+    profession: jobs[0].Contract.Contractor.profession,
+  });
+});
+
 module.exports = app;
