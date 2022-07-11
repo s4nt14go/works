4bda07cb7254c02b2137f190ac952191d574c1f8
s4nt14go <santiagopergamo@gmail.com>
2022-07-11 14:21:26 -0300

    feat: return sensible error message when profile_id header isn't set

--- src/middleware/getProfile.js
+++ src/middleware/getProfile.js
@@ -1,5 +1,13 @@
 const getProfile = async (req, res, next) => {
   const profile_id = req.get('profile_id');
+  const profileId = Number(profile_id);
+  if (isNaN(profileId))
+    return res
+      .status(401)
+      .send({
+        message: `Invalid 'profile_id' header: ${profile_id}`,
+      })
+      .end();
   const { Profile } = req.app.get('models');
   const profile = await Profile.findOne({
     where: { id: profile_id },
