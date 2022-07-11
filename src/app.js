b49ff0a6e482711ae02928c7962b1825143c479d
s4nt14go <santiagopergamo@gmail.com>
2022-07-11 00:18:11 -0300

    feat: client deposit

--- src/app.js
+++ src/app.js
@@ -143,4 +143,76 @@ app.post('/jobs/:job_id/pay', getProfile, async (req, res) => {
   res.json();
 });
 
+/**
+ * Client deposit
+ * @returns void
+ */
+app.post('/balances/deposit/:userId', async (req, res) => {
+  const { userId } = req.params;
+  const { deposit: depositRaw } = req.body;
+
+  const deposit = Number(depositRaw);
+  if (isNaN(deposit))
+    return res
+      .status(400)
+      .send({
+        message: `Deposit isn't a number: ${depositRaw}`,
+      })
+      .end();
+
+  const { Profile, Contract, Job } = req.app.get('models');
+
+  const client = await Profile.findOne({
+    where: {
+      id: userId,
+      type: 'client',
+    },
+  });
+  if (!client)
+    return res
+      .status(401)
+      .send({
+        message: `No client found with id: ${userId}`,
+      })
+      .end();
+
+  const contracts = await Contract.findAll({
+    where: {
+      ClientId: client.id,
+    },
+    include: Job,
+  });
+
+  let jobsToPay = 0;
+  contracts.map((contract) => {
+    contract.Jobs.map((job) => {
+      if (!job.paid) {
+        jobsToPay += job.price;
+      }
+    });
+  });
+
+  // A client can't deposit more than 25% his total of jobs to pay
+  const maxDeposit = jobsToPay / 4;
+  if (maxDeposit === 0)
+    return res
+      .status(400)
+      .send({
+        message: `Client doesn't have jobs to pay`,
+      })
+      .end();
+
+  if (deposit > maxDeposit)
+    return res
+      .status(400)
+      .send({
+        message: `Client can't deposit more than 25% his total of jobs to pay. Maximum deposit allowed: ${maxDeposit}`,
+      })
+      .end();
+
+  await client.increment({ balance: deposit });
+
+  res.json();
+});
+
 module.exports = app;
