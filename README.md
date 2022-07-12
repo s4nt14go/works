b53dfe9fea7d8cc684dc2a8d054ba4790c81c890
s4nt14go <santiagopergamo@gmail.com>
2022-07-12 19:21:16 -0300

    refactor: move GetByIdController tests to a separate file
    Update README.md

--- README.md
+++ README.md
@@ -88,7 +88,8 @@ Run them with `npm run test-unit`
 ### Integration Tests
 
 - `src/modules/balances/DepositController.int.js`
-- `src/modules/contracts/contractsController.int.js`
+- `src/modules/contracts/GetByIdController.int.js`
+- `src/modules/contracts/GetContractsController.int.js`
 
 Run them with `npm run test-int`
 
---
+++ src/modules/contracts/GetContractsController.int.js
@@ -0,0 +1,23 @@
+const request = require("supertest");
+const app = require("../../app");
+
+it(`should return non terminated contracts for the profile`, async () => {
+  const profile_id = 1;
+
+  const response = await request(app)
+    .get(`/contracts`)
+    .set('profile_id', `${profile_id}`);
+
+  expect(response.status).toBe(200);
+  expect(response.body.contracts).toStrictEqual(
+    expect.arrayContaining([
+      expect.objectContaining({
+        status: expect.not.stringContaining('terminated'),
+      }),
+    ])
+  );
+  for (let i = 0; i < response.body.length; i++) {
+    const contract = response.body[i];
+    expect([contract.ContractorId, contract.ClientId]).toContain(profile_id);
+  }
+});
