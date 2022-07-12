d98efb082b131e8d577e58037ea0983828ff5e41
s4nt14go <santiagopergamo@gmail.com>
2022-07-12 19:48:16 -0300

    feat: include time in responses only for server error

--- src/shared/BaseController.js
+++ src/shared/BaseController.js
@@ -19,27 +19,36 @@ class BaseController {
     } catch (err) {
       if (this.transaction) await this.transaction.rollback();
       console.log(`An unexpected error occurred`, err);
-      await this.fail(new AppError());
+      await this.serverError(new AppError());
     }
   }
 
   async ok(result) {
     if (this.transaction) await this.transaction.commit();
-    this.res.json({
-      ...result,
-      time: new Date().toJSON(),
-    });
+    this.res.json(result);
   }
 
   async fail(error) {
     if (this.transaction) await this.transaction.rollback();
-    const { httpStatus, message, type, time } = error;
+    const { httpStatus, message, type } = error;
     this.res
       .status(httpStatus)
       .send({
         message,
         errorType: type,
-        time,
+      })
+      .end();
+  }
+
+  async serverError(error) {
+    if (this.transaction) await this.transaction.rollback();
+    const { message, type } = error;
+    this.res
+      .status(500)
+      .send({
+        message,
+        errorType: type,
+        time: new Date().toJSON(),
       })
       .end();
   }
--- src/shared/BaseError.js
+++ src/shared/BaseError.js
@@ -3,7 +3,6 @@ class BaseError {
     this.type = this.constructor.name;
     this.message = message;
     this.httpStatus = httpStatus;
-    this.time = new Date().toJSON();
   }
 }
 
